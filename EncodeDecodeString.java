9fe4661d385172abbcc2c8d23894e6083a6c50c8
Andreixzc <andrei.massaini@hotmail.com>
2023-08-30 23:01:56 -0300

    product

---
+++ EncodeDecodeString.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class EncodeDecodeString {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String encode(List<String> strs) {
+        
+        
+        
+        return "";
+        // write your code here
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    public static List<String> decode(String str) {
+        
+        
+        return new List<String> e();
+        // write your code here
+    }
+}
--- ProductExcpetSelf.java
+++ ProductExcpetSelf.java
@@ -2,21 +2,27 @@ import java.util.Arrays;
 
 public class ProductExcpetSelf {
     public static void main(String[] args) {
-        int[] vet = {1,2,3,4,5};
-        System.out.println(Arrays.toString(vet));
+        int[] vet = { 1, 2, 3, 4 };
+        System.out.println(Arrays.toString(productExceptSelf(vet)));
+
     }
 
     public static int[] productExceptSelf(int[] nums) {
-        int[] arr = new int[nums.length];
-        int right = 1, left = 1;
+        int[] preFix = new int[nums.length];
+        int[] posFix = new int[nums.length];
+        int[] output = new int[nums.length];
+        preFix[0] = 1;
+        posFix[nums.length - 1] = 1;
+        for (int i = 1; i < preFix.length; i++) {
+            preFix[i] = nums[i - 1] * preFix[i - 1];
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            posFix[i] = nums[i + 1] * posFix[i + 1];
+        }
         for (int i = 0; i < nums.length; i++) {
-            arr[i] = left;
-            left *= nums[i];
+            output[i] = preFix[i] * posFix[i];
         }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            arr[i] *= right;
-            right *= nums[i];
-        }
-        return arr;
+        return output;
     }
+
 }
