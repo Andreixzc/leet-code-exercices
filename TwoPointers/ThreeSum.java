94810d4a8c9ad0f95afd688439d37e5b49aaea09
Andreixzc <andrei.massaini@hotmail.com>
2023-08-31 11:52:17 -0300

    more

---
+++ TwoPointers/ThreeSum.java
@@ -0,0 +1,17 @@
+package TwoPointers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        int j = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int complemento = nums[i] + nums[j];
+        }
+    }
+}
