28f5ef9856f1f4903cd8d46e2af30811cf40f72c
Andreixzc <andrei.massaini@hotmail.com>
2023-09-02 10:43:21 -0300

    containsDuplicate 2

---
+++ Random/ContainsDuplicateII.java
@@ -0,0 +1,33 @@
+package Random;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class ContainsDuplicateII {
+    public static void main(String[] args) {
+        int[] vet = { 1, 2, 3, 1, 2, 3 };
+        // System.out.println(containsNearbyDuplicate2(vet, 2));
+        System.out.println(Math.abs(-12));
+    }
+
+    public static boolean containsNearbyDuplicate2(int[] nums, int k) {
+    
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                if (Math.abs(hashMap.get(nums[i])-i)<= k) {
+                    return true;
+                }
+            }
+            hashMap.put(nums[i], i);
+        }
+        return false;
+    
+    
+    }
+    
+}
