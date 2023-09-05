7ad85fb2ed10a896b412478d66f546453f568a18
Andreixzc <andrei.massaini@hotmail.com>
2023-09-05 10:47:44 -0300

    Parhentseis

--- Random/Parhentesis.java
+++ Random/Parhentesis.java
@@ -11,15 +11,24 @@ public class Parhentesis {
         if (s.length() % 2 != 0) {
             return false;
         }
+
         Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '[' ||s.charAt(i) == '{' || s.charAt(i) == '(' ) {
-                stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ']' ||s.charAt(i) == '}' || s.charAt(i) == ')') {
-                
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(' || c == '}' && top != '{' || c == ']' && top != '[') {
+                    return false;
+                }
             }
         }
 
-        return true;
+        return stack.isEmpty();
     }
 }
