a3d094759a224694acf510740fe5856378b336cb
Andreixzc <andrei.massaini@hotmail.com>
2023-09-05 08:54:13 -0300

    3sumCheated

--- TwoPointers/ThreeSum.java
+++ TwoPointers/ThreeSum.java
@@ -8,53 +8,39 @@ import java.util.List;
 
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] numeros = { 1, 2, 3, 4, 5 };
-        threeSum(numeros);
-
-    }
-    public static int binarySearch(int[] vet, int left, int right, int target, int flag) {
-        if (left > right) {
-            return flag;
-        }
-        int mid = left + (right - left) / 2;
-        if (vet[mid] == target) {
-            return mid;
-        }
-
-        if (vet[mid] < target) {
-            return binarySearch(vet, mid + 1, right, target, flag);
-        } else {
-            return binarySearch(vet, left, mid - 1, target, flag);
-        }
-
+        int[] numeros = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(numeros));
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> output = new ArrayList<>();
-        HashSet<Integer[]> hash = new HashSet<>();
         Arrays.sort(nums);
-        int flag = nums[0] - 1;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] != nums[j]) {
-                    int complemento = (nums[i] + nums[j]) * -1;
-                    int found = binarySearch(nums, 0, nums.length - 1, complemento, flag);
-                    if (found != flag) {
-                        Integer[] insert = new Integer[3];
-                        insert[0] = nums[i];
-                        insert[1] = nums[j];
-                        insert[2] = found;
-                        if (!hash.contains(insert)) {
-                            hash.add(insert);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int left = i + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == 0) {
+                        output.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
                         }
-
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (sum < 0) {
+                        left++;
+                    } else {
+                        right--;
                     }
-
                 }
             }
         }
-        System.out.println(hash.toString());
         return output;
-
     }
+
 }
