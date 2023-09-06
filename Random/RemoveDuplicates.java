5215023638563db0269a8d8923c36642c50dd420
Andreixzc <andrei.massaini@hotmail.com>
2023-09-06 12:00:15 -0300

    xd

--- Random/RemoveDuplicates.java
+++ Random/RemoveDuplicates.java
@@ -1,27 +1,29 @@
 package Random;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] vet = {0,0,1,1,1,2,2,3,3,4};
-        removeDuplicates(vet);
+        int[] vet = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(vet));
 
     }
+
+
     public static int removeDuplicates(int[] nums) {
-        //[0,0,1,1,1,2,2,3,3,4]
-        int k = 0;
-        for (int i = 0; i < nums.length-1; i++) {
-             int j = i+1;
-            while (nums[i] == nums[j]) {
-                j++;
-                k++; // k = indice da ultima repetição do numnero atual anlisado
-            }
-            System.out.println("Iteracao: " + i + " K = :" + k);
-            nums[i+1] = nums[k+1];
+        if (nums.length == 0) {
+            return 0;
         }
-
-        return 0;
+        int k = 1; 
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i]; 
+                k++;
+            }
+        }
+        return k;
     }
+    
 
 }
