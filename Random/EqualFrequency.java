e006df6eec01f9e11e002957884fb69f210eb549
Andreixzc <andrei.massaini@hotmail.com>
2023-09-18 11:33:46 -0300

    update

--- Random/EqualFrequency.java
+++ Random/EqualFrequency.java
@@ -9,47 +9,26 @@ import javax.swing.RowFilter.Entry;
 public class EqualFrequency {
 
     public static void main(String[] args) {
-        equalFrequency("bac");
-        //criar mapa de frequencia
-        //contar quantos valores diferentes
-        //Se existir mais de um valor diferente, retorne falso
-        //Se apenas existir um valor diferente, verificar se a frequencia dele é apenas 1 unidade maior do que seus diferentes.
-        //se sim, retornar true caso não retornar falso.
+        System.out.println(equalFrequency("bac"));
     }
 
     public static boolean equalFrequency(String word) {
-        HashMap<Character, Integer> hash = new HashMap<>();
-        int difCount = 0;
-        char difKey = '\0'; // Inicialize com um valor nulo
+        int current = 0;
         int prev = 0;
-
         for (int i = 0; i < word.length(); i++) {
-            if (!hash.containsKey(word.charAt(i))) {
-                hash.put(word.charAt(i), 1);
-            } else {
-                hash.put(word.charAt(i), hash.get(word.charAt(i)) + 1);
-            }
-        }
-
-        prev = hash.get(word.charAt(0));
-
-        for (Map.Entry<Character, Integer> set : hash.entrySet()) {
-            if (set.getValue() != prev) {
-                if (Math.abs(set.getValue() - prev) != 1) {
-                    return false;
+            for (int j = i + 1; j < word.length(); j++) {
+                //nao pode comecar j com i +1 , pq desse jeito ignora-sse os outros chars
+                HashMap<Character, Integer> hashMap = new HashMap<>();
+                if (!hashMap.containsKey(word.charAt(j))) {
+                    hashMap.put(word.charAt(j), 1);
+                } else {
+                    hashMap.put(word.charAt(i), hashMap.get(word.charAt(i)) + 1);
                 }
-                difKey = set.getKey();
-                difCount++;
             }
-            if (difCount > 1) {
-                return false;
-            }
-            prev = set.getValue();
         }
-        if (difCount == 0) {
-            return false;
-            
-        }
-        return true;
+        return false;
     }
+
+
+
 }
