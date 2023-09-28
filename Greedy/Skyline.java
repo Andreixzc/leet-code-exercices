7f0dcff56764c12cfa4c677d2b3e3267df446abc
Andreixzc <andrei.massaini@hotmail.com>
2023-09-28 11:57:07 -0300

    sky

---
+++ Greedy/Skyline.java
@@ -0,0 +1,16 @@
+package Greedy;
+
+public class Skyline {
+    public static void main(String[] args) {
+        
+
+    }
+    public static int maxIncreaseKeepingSkyline(int[][] grid) {
+        //Encontrar o maior valor de cada coluna, e o maior de cada linha.
+        //Se o numero da celula atual for == o maior, ignore, se não atribua o menor valor dentre os dois - 1.
+        
+
+
+        return 0;
+    }
+}
