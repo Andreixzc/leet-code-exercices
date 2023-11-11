8de4e1d1b4c93267381e6c061f97a24500b6e18a
Andreixzc <andrei.massaini@hotmail.com>
2023-11-11 03:30:34 -0300

    deleting garbage

--- Random/SubstringIndex.java
+++
@@ -1,8 +0,0 @@
-package Random;
-
-public class SubstringIndex {
-    public static void main(String[] args) {
-        String str = "leeto";
-        System.out.println(str.indexOf("leetcode"));
-    }
-}
