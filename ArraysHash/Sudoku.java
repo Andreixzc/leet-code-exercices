167f15aa68b88567e5168febc601399d03302f1c
Andreixzc <andrei.massaini@hotmail.com>
2023-11-12 15:18:42 -0300

    ds

--- ArraysHash/Sudoku.java
+++ ArraysHash/Sudoku.java
@@ -1,40 +1,75 @@
 package ArraysHash;
 
+import java.util.HashSet;
+
 public class Sudoku {
     public static void main(String[] args) {
-        char[][] sudoku = {
-                { '5', '3', '0', '0', '7', '0', '0', '0', '0' },
-                { '6', '0', '0', '1', '9', '5', '0', '0', '0' },
-                { '0', '9', '8', '0', '0', '0', '0', '6', '0' },
-                { '8', '0', '0', '0', '6', '0', '0', '0', '3' },
-                { '4', '0', '0', '8', '0', '3', '0', '0', '1' },
-                { '7', '0', '0', '0', '2', '0', '0', '0', '6' },
-                { '0', '6', '0', '0', '0', '0', '2', '8', '0' },
-                { '0', '0', '0', '4', '1', '9', '0', '0', '5' },
-                { '0', '0', '0', '0', '8', '0', '0', '7', '9' }
+        char[][] board = {
+                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
         };
-        verifyQuadrant(sudoku, 0);
+        System.out.println(isValidSudoku(board));
     }
 
-    // public static boolean isValidSudoku(char[][] board) {
-    // for (int i = 0; i < board.length; i++) {
-    // for (int j = 0; j < board[0].length; j++) {
-    // System.out.print(board[i][j] + " ");
-    // }
-    // System.out.println();
-    // }
-    // return false;
-    // }
-
-    public static void verifyQuadrant(char[][] board, int quadrantNumber) {
-        // 0 - 3
-        // 3 - 6
-        // 6 - 9
-        for (int i = 3; i < 6; i++) {
-            for (int j = 3; j < 6; j++) {
-                System.out.print(board[i][j] + " ");
+    public static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            if (!verificaLinha(board, i) || !verificaColuna(board, i) || !verificaSubMatriz(board, i)) {
+                return false;
             }
-            System.out.println();
         }
+        return true;
+    }
+    
+    public static boolean verificaLinha(char[][] board, int row) {
+        HashSet<Character> set = new HashSet<>();
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] != '.') {
+                if (set.contains(board[row][j])) {
+                    return false;
+                } else {
+                    set.add(board[row][j]);
+                }
+            }
+        }
+        return true;
+    }
+    
+    public static boolean verificaColuna(char[][] board, int col) {
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] != '.') {
+                if (set.contains(board[i][col])) {
+                    return false;
+                } else {
+                    set.add(board[i][col]);
+                }
+            }
+        }
+        return true;
+    }
+    
+    public static boolean verificaSubMatriz(char[][] board, int subMatrizNumber) {
+        int subMatrizRow = (subMatrizNumber / 3) * 3;
+        int subMatrizCol = (subMatrizNumber % 3) * 3;
+        HashSet<Character> set = new HashSet<>();
+        for (int i = subMatrizRow; i < subMatrizRow + 3; i++) {
+            for (int j = subMatrizCol; j < subMatrizCol + 3; j++) {
+                if (board[i][j] != '.') {
+                    if (set.contains(board[i][j])) {
+                        return false;
+                    } else {
+                        set.add(board[i][j]);
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
---
+++ InterviewQuestions/Produc.java
@@ -0,0 +1,42 @@
+package InterviewQuestions;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Produc {
+
+    public static void main(String[] args) {
+        String word = "mouse";
+        String[] products = { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
+       System.out.println(suggestedProducts(products, word));
+        // [["mobile","moneypot","monitor"],["mobile","moneypot","monitor"],
+    }
+    public static List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        StringBuilder sb = new StringBuilder();
+        List<List<String>> output = new ArrayList<>();
+
+        for (int i = 0; i < searchWord.length(); i++) {
+            sb.append(searchWord.charAt(i));
+            List<String> matches = new ArrayList<>();
+
+            for (int j = 0; j < products.length; j++) {
+                if (products[j].startsWith(sb.toString())) {
+                    matches.add(products[j]);
+                }
+            }
+            Collections.sort(matches);
+            List<String> aux = new ArrayList<>();
+            for (int j = 0; j < 3 && j < matches.size(); j++) {
+                aux.add(matches.get(j));
+            }
+    
+            output.add(aux);
+            matches.clear();
+
+            
+        }
+
+        return output;
+    }
+
+}
