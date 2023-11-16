3f203efb2d7eca7cad35e7d18ac22b0127eb5746
Andreixzc <andrei.massaini@hotmail.com>
2023-11-16 04:58:36 -0300

    trying to implement without recursion and failing

--- InterviewQuestions/PaintBucket.java
+++ InterviewQuestions/PaintBucket.java
@@ -8,24 +8,22 @@ public class PaintBucket {
                 { '.', '#', '#', '#', '.', '.' },
                 { '.', '.', '.', '.', '.', '.' }
         };
-        
-        printMatriz(matriz);
-        System.out.println("---------------");
-        colorir(matriz, 0, 0, 'O');
 
+        printMatriz(matriz);
+        paintRecursive(matriz, 0, 0, 'O');
         printMatriz(matriz);
 
     }
 
-    public static void colorir(char[][] matriz, int row, int col, char color) {
+    public static void paintRecursive(char[][] matriz, int row, int col, char color) {
         if (row < 0 || row >= matriz.length || col < 0 || col >= matriz[0].length || matriz[row][col] != '.') {
             return;
         }
         matriz[row][col] = color;
-        colorir(matriz, row - 1, col, color);
-        colorir(matriz, row + 1, col, color);
-        colorir(matriz, row, col - 1, color);
-        colorir(matriz, row, col + 1, color);
+        paintRecursive(matriz, row - 1, col, color);
+        paintRecursive(matriz, row + 1, col, color);
+        paintRecursive(matriz, row, col - 1, color);
+        paintRecursive(matriz, row, col + 1, color);
     }
 
     public static void printMatriz(char[][] matriz) {
@@ -36,7 +34,5 @@ public class PaintBucket {
             System.out.println();
         }
     }
-
-
 
 }
