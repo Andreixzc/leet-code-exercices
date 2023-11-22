ee213d62ae5dc32ce2076f8aad1c53d41b30c957
Andreixzc <andrei.massaini@hotmail.com>
2023-11-21 21:53:34 -0300

    temperature

---
+++ Java/Stack/DailyTemp.java
@@ -0,0 +1,18 @@
+package Stack;
+
+import java.util.Stack;
+
+public class DailyTemp {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            
+        }
+
+        return temperatures;
+    }
+}
