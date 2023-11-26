11bc33e613b8cd561ebdcf6661facbc69b0febb8
Andrei <andrei.massaini@hotmail.com>
2023-11-26 13:36:38 -0300

    merge arrays

--- Java/InterviewQuestions/MergArr.java
+++ Java/InterviewQuestions/MergArr.java
@@ -22,22 +22,22 @@ public class MergArr {
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        int j = m - 1;
+        int k = n - 1;
+    
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (k < 0) {
+                break;
+            }
+    
+            if (j >= 0 && nums1[j] > nums2[k]) {
+                nums1[i] = nums1[j];
+                j--;
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[i] = nums2[k];
+                k--;
             }
         }
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
 
-    }
-
+}
 }
